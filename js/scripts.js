4c62dc55288a18a9087a64990d4c92ce22fe3b70
Michael Arrington and Susannah Lowe <susannah@>
2017-10-02 15:52:08 -0700

    add form to html and js

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,25 @@
+//business logic
+
+
+//front end logic
 $(document).ready(function() {
   $("#quiz").submit(function(event) {
     event.preventDefault();
+    var fluidComfortArr = [];
+    $("input:checkbox[name=fluid-comfort]:checked").each(function(){
+      var fluidComfort = $(this).val();
+      fluidComfortArr.push(fluidComfort);
+    });
+      $("#output").text(fluidComfortArr);
 
 
   });
+  $("#quizNav1").click(function(event) {
+    $("#quiz1").hide();
+    $("#quiz2").show();
+  })
+  $("#quizNav2").click(function(event) {
+    $("#quiz2").hide();
+    $("#quiz3").show();
+  })
 });
