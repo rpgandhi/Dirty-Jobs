1fc2723391073d8fe297515e84955e366b8c0117
Jessie Waite and Michael Arrington <michael@>
2017-10-05 10:59:33 -0700

    Remove z index from slider

--- js/scripts.js
+++ js/scripts.js
@@ -33,7 +33,7 @@ var dirty = ["Garbage Collecter", "Sewer Inspecter", "Septic Tank Servicer"];
 
 //front end logic
 $(document).ready(function() {
-  //$('.slider').slider();
+  $('.slider').slider();
   $("#quiz").submit(function(event) {
     event.preventDefault();
   });
